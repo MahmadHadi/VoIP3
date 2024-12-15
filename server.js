f56b208700a354dc2db432318e07bd867a50a1a0
MahmadHadi <nayanihadi97@gmail.com>
2024-12-15 22:02:18 +0530

    update last

--- server.js
+++ server.js
@@ -1,12 +1,47 @@
+// const express = require("express");
+// const app = express();
+// const server = require("http").Server(app);
+// const io = require("socket.io")(server);
+// const { v4: uuidv4 } = require("uuid");
+
+// // const port = 3000;
+// const PORT = process.env.PORT || 3000; // Default to 3000 for local dev
+
+// app.set("view engine", "ejs");
+// app.use(express.static("public"));
+
+// app.get("/", (req, resp) => {
+//     resp.redirect(`/${uuidv4()}`);
+// });
+
+// app.get("/:room", (req, resp) => {
+//     resp.render("room", { roomId: req.params.room });
+// });
+
+// io.on("connection", (socket) => {
+//     socket.on("join-room", (roomId, userId) => {
+//         console.log(roomId, userId);
+//         socket.join(roomId);
+//         socket.to(roomId).broadcast.emit("user-connected", userId);
+
+//         socket.on("disconnect", () => {
+//             socket.to(roomId).broadcast.emit('user-disconnected', userId)
+//         })
+//     });
+
+// });
+
+// server.listen(PORT);
+
+// ! ---
+
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
-const io = require("socket.io")(server);
+const socketIo = require("socket.io");
 const { v4: uuidv4 } = require("uuid");
 
-// const port = 3000;
-const PORT = process.env.PORT || 3000; // Default to 3000 for local dev
-
+const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -19,6 +54,12 @@ app.get("/:room", (req, resp) => {
     resp.render("room", { roomId: req.params.room });
 });
 
+const io = socketIo(server, {
+    cors: {
+        origin: "*",
+    },
+});
+
 io.on("connection", (socket) => {
     socket.on("join-room", (roomId, userId) => {
         console.log(roomId, userId);
@@ -26,10 +67,11 @@ io.on("connection", (socket) => {
         socket.to(roomId).broadcast.emit("user-connected", userId);
 
         socket.on("disconnect", () => {
-            socket.to(roomId).broadcast.emit('user-disconnected', userId)
-        })
+            socket.to(roomId).broadcast.emit("user-disconnected", userId);
+        });
     });
-
 });
 
-server.listen(PORT);
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
